1b9e9fc8d8751cafbabcb80e0efdcad9961a15d7
Indrabot26 <96617684+Indrabot26@users.noreply.github.com>
2021-12-28 23:29:07 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -24,7 +24,7 @@ router.get('/login', (req, res) => {
 })
 
 router.get('/login', (req, res) => {
-             res.sendFile(__path + '/viwes/login.html')
+             res.sendFile(__path + '/views/login.html')
 })
 
 router.get('/config', (req, res) => {
